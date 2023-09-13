043d712bc3315ca105b01e664ae4e123b200fd8c
APascale <andreacpascale@gmail.com>
2023-09-13 17:42:25 -0400

    python

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # aws
-CloudFormation templates and misc ETL code
+
+## Environment Build
+This repo includes cloudformation templates for designing a custom private cloud infrastructure. Options such as database and EC2 configuration are parameterized in a stack template.
+
+## Alpha Vantage API
+This suite of APIs provide global equity data in 4 different temporal resolutions: daily, weekly, monthly, and intraday, with 20+ years of historical depth.
+
+1. Core Time Series Stock Data APIs
+2. Alpha Intelligence™
+3. Fundamental Data
+4. Physical and Digital/Crypto Currencies
+5. Commodities
+6. Economic Indicators
+7. Technical Indicators
+
+This repo features code samples in R and Python for accessing the API and storing data in an S3 bucket.
+
+## ETL Pipelines
+Data cleansing, normalization, and batch uploads to a PostgreSQL database.
--- api_call.R
+++ api_call.R
@@ -8,53 +8,68 @@ library(janitor)
 library(aws.s3)
 
 config <- yaml.load_file('config.yml')
-
-params <- list("X-RapidAPI-Key" = config$apiserver['apikey'][[1]],
-              "X-RapidAPI-Host" = config$apiserver['host'][[1]])
-
 ticker <- "MSFT"
 
-queryString <- list("function" = "TIME_SERIES_DAILY",
-                    "symbol" = ticker,
-                    "outputsize" = "compact",
-                    "datatype" = "json")
+# Establish API call parameters
+url <- config$apiserver['requesturl'][[1]]
+headers <- list("X-RapidAPI-Key" = config$apiserver['apikey'][[1]],
+              "X-RapidAPI-Host" = config$apiserver['host'][[1]])
 
-req <- request(config$apiserver['requesturl'][[1]]) %>%
-  req_headers(!!!params) %>% 
-  req_url_query(!!!queryString)
+params <- list("function" = "TIME_SERIES_DAILY",
+              "symbol" = ticker,
+              "outputsize" = "compact",
+              "datatype" = "json")
 
+# Build request connection
+req <- request(url) %>%
+  req_headers(!!!headers) %>% 
+  req_url_query(!!!params)
+
+# View request syntax
 req  %>%
   req_dry_run()
 
+# GET request
 resp <- req_perform(req)
 
 resp %>% resp_status_desc()
 resp %>% resp_content_type()
 
+# convert response to JSON
 content <- httr2::resp_body_json(resp)
+
+# extract metadata
 symbol <- content$`Meta Data`$`2. Symbol`
 last_updated <- as.Date(content$`Meta Data`$`3. Last Refreshed`)
 
+# get JSON body content
 data <- content$`Time Series (Daily)`
 
+# convert JSON to dataframe
 df <- do.call(rbind, lapply(data, data.frame))
+
+# clean column names
 cols <- as.vector( sapply(
           sapply(make_clean_names(colnames(df)), function(x) strsplit(x, '_')),
             function(y) y[length(y)] )
           )
-
 colnames(df) <- cols
+
+# change data to type numeric
+df <- df %>% mutate(across(where(is.character), as.numeric))
+
+# make index with observation date into a new column with datetime format
 df <- cbind('obs_date' = rownames(df), df)
 rownames(df) <- NULL
-df <- as_tibble(df)
-
 df$obs_date <- as.Date(df$obs_date)
-df <- df %>% mutate(across(where(is.character), as.numeric))
+
+# copy stock ticker into new column
 df <- cbind('ticker' = symbol, df)
 
+head(df, n=10)
 sapply(df, class)
-df
 
+# Establish connection to AWS environment
 Sys.setenv(
   AWS_ACCESS_KEY_ID = config$awsaccount['accesskey'][[1]],
   AWS_SECRET_ACCESS_KEY = config$awsaccount['secretkey'][[1]],
@@ -63,6 +78,7 @@ Sys.setenv(
 
 bucket <- "stocks-daily-ohlc"
 
+# write dataframe to s3 bucket as CSV
 s3write_using(df,
               FUN = write.csv,
               bucket = bucket,
